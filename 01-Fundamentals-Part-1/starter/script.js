553fe20e847fc948997119a4712fa6e3e293ee17
geberma <manuel.gebert@kvb.de>
2026-03-13 11:53:55 +0100

    fix: HTML und JS aktualisiert

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,17 @@
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log('Jonas');
+console.log(23);
+
+let firstName = 'Bob';
+
+console.log(firstName);
+
+let country = "Germany";
+let continent = "Europe";
+let population = 83;
+
+console.log(country);
+console.log(continent);
+console.log(population);
