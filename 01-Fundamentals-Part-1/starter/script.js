20f3e9557e441143fea27b085ec8461fe2bef131
geberma <manuel.gebert@kvb.de>
2026-03-27 08:49:54 +0100

    refactor: Code aufgeräumt und Kommentare hinzugefügt

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,17 +1,63 @@
-let js = 'amazing';
-console.log(40 + 8 + 23 - 10);
+// let js = 'amazing';
+// console.log(40 + 8 + 23 - 10);
+//
+// console.log('Jonas');
+// console.log(23);
+//
+// let firstName = 'Bob';
+//
+// console.log(firstName);
+//
+// let country = "Germany";
+// let continent = "Europe";
+// let population = 83;
+//
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+//
+// let year;
+// console.log(year);
+// console.log(typeof year);
+//
+// year = 1991;
+// console.log(year);
+// console.log(typeof year);
+//
+// console.log(typeof null);
 
-console.log('Jonas');
-console.log(23);
+// let age = 30;
+// age = 45;
 
-let firstName = 'Bob';
+const birthYear = 1991;
+// birthYear = 1990;
 
-console.log(firstName);
+// const job;
 
-let country = "Germany";
-let continent = "Europe";
-let population = 83;
+// lastName = "Gebert";//dont use this! global var!
+// console.log(lastName);
 
-console.log(country);
-console.log(continent);
-console.log(population);
+
+const now = 2026;
+const ageGebi = now - 1983;
+const ageZoe = now - 2007;
+console.log(ageGebi, ageZoe);
+
+console.log(ageGebi *2, ageGebi / 10, 2 ** 3)
+
+const firstName = 'Manuel'
+const lastName = 'Gebert';
+
+console.log(firstName + ' ' +  lastName);
+
+
+let x = 10 + 5;
+x+= 10; // x = x + 10
+
+console.log(x);
+
+
+console.log(ageGebi > ageZoe);
+
+
+
